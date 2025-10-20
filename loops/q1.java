69244716e2833776f02f1f6aac33f9e4171d0aba
Praveen <236072063+0PxF@users.noreply.github.com>
2025-10-20 03:41:14 +0000

    Create q1.java

---
+++ loops/q1.java
@@ -0,0 +1,13 @@
+//Program to display square and cube for first n natural numbers
+import  java.util.Scanner;
+public  class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.println("Enter number");
+        n=sc.nextInt();
+        System.out.println("The square  and cubes of natural numbers are:");
+        for(int i=1;i<=n;i++)
+            System.out.println("Square:"+(i*i)+" Cube:"+(i*i*i));
+    }
+}
